ec1c3700579184abc68a907edf1d470e6516594c
Pierre Nyssen <pierre.nyssen@protonmail.com>
2021-03-30 17:17:25 +0200

    change svg color, edit menu style and add hero image

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@
 document.getElementById('navBtn').addEventListener('click',function myFunction() {
     this.classList.toggle("change");
    });
+
 /*Element of the Menu*/
+
 document.getElementById('navBtn').addEventListener('click',function scroolMenu(){
     var element = document.getElementById("menu");
     if(element.style.display === "block"){
